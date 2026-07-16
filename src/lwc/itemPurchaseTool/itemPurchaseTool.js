2099f6650cf05865250634866a47005b72d620e2
robbyroboter <rosticzuev2002@mail.ru>
2026-07-16 22:56:55 +0500

    Обновление LWC и Apex: Добавление отображения новых данных об аккаунте.

--- src/lwc/itemPurchaseTool/itemPurchaseTool.js
+++ src/lwc/itemPurchaseTool/itemPurchaseTool.js
@@ -1,4 +1,26 @@
-import {LightningElement} from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+
+import getAccountInfo from '@salesforce/apex/AccountController.getAccountInfo';
 
 export default class ItemPurchaseTool extends LightningElement {
+
+    @api recordId;
+
+    account;
+    error;
+
+    @wire(getAccountInfo, { accountId: '$recordId' })
+    wiredAccount({ data, error }) {
+
+        if (data) {
+
+            this.account = data;
+            this.error = undefined;
+        }
+        else if (error) {
+
+            this.error = error;
+            this.account = undefined;
+        }
+    }
 }
